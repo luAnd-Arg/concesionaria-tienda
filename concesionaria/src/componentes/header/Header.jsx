0b4159838c1d0859115bdfb28de3a1926e6f1409
luAnd-Arg <luccasmandrada1994@gmail.com>
2023-10-07 02:57:56 -0300

    nueva version

---
+++ concesionaria/src/componentes/header/Header.jsx
@@ -0,0 +1,10 @@
+import NavBar from "../navbar/Navbar";
+function header (){
+    return (
+        <NavBar title = {"Concesionaria ProMotor"} subtitulo = {"Bienvenidos a la pagina oficial"} />
+    )
+    }
+
+
+export default header;
+//importar el logo
